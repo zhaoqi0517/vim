5db9c6af747a1c0ad23684f76fea21dc1d076987
Qi Zhao <qi.zhao@sf-express.com>
2018-02-27 13:11:33 +0800

    fix some bugs.

--- link.sh
+++ link.sh
@@ -13,18 +13,18 @@ rm ~/.subversion
 rm ~/.vim
 rm ~/.vimrc
 
-ln -s ~/Home/.curlrc            ~/.curlrc
-ln -s ~/Home/.gitconfig         ~/.gitconfig
-ln -s ~/Home/.gitignore_global  ~/.gitignore_global
-ln -s ~/Home/.hgext             ~/.hgext
-ln -s ~/Home/.hgignore_global   ~/.hgignore_global
-ln -s ~/Home/.hgrc              ~/.hgrc
-ln -s ~/Home/.kdiff3rc          ~/.kdiff3rc
-ln -s ~/Home/.mavenrc           ~/.mavenrc
-ln -s ~/Home/.npmrc             ~/.npmrc
-ln -s ~/Home/.profile           ~/.profile
-ln -s ~/Home/.ssh               ~/.ssh
-ln -s ~/Home/.subversion        ~/.subversion
-ln -s ~/Home/.vim               ~/.vim
-ln -s ~/Home/.vimrc             ~/.vimrc
+ln -s ~/Documents/vim/.curlrc            ~/.curlrc
+ln -s ~/Documents/vim/.gitconfig         ~/.gitconfig
+ln -s ~/Documents/vim/.gitignore_global  ~/.gitignore_global
+ln -s ~/Documents/vim/.hgext             ~/.hgext
+ln -s ~/Documents/vim/.hgignore_global   ~/.hgignore_global
+ln -s ~/Documents/vim/.hgrc              ~/.hgrc
+ln -s ~/Documents/vim/.kdiff3rc          ~/.kdiff3rc
+ln -s ~/Documents/vim/.mavenrc           ~/.mavenrc
+ln -s ~/Documents/vim/.npmrc             ~/.npmrc
+ln -s ~/Documents/vim/.profile           ~/.profile
+ln -s ~/Documents/vim/.ssh               ~/.ssh
+ln -s ~/Documents/vim/.subversion        ~/.subversion
+ln -s ~/Documents/vim/.vim               ~/.vim
+ln -s ~/Documents/vim/.vimrc             ~/.vimrc
 
